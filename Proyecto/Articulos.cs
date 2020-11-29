3369883eb4cb9b352943dbad6d7415f940b92fb6
Gerardo Solis Garcia <gsolisg1500@alumno.ipn.mx>
2020-11-29 15:07:00 -0600

    Get y Set de la clase articulos

--- Proyecto/Articulos.cs
+++ Proyecto/Articulos.cs
@@ -8,5 +8,47 @@ namespace Proyecto
 {
     class Articulos
     {
+        private float precio;
+        private String descripcion;
+        private int clvArticulo;
+        private int cantDisponible;
+        private String marca;
+
+
+        public float Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+
+        }
+
+        public String Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+        
+        public int ClvArticulo
+        {
+            get { return clvArticulo; }
+            set { clvArticulo = value; }
+        }
+
+        public int CantDisponible
+        {
+            get { return cantDisponible; }
+            set { cantDisponible = value; }
+        }
+
+        public String Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+            
+   
+
+
+
     }
 }
